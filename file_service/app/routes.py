3f9b113419e434174c696be020599719d21b163f
Samy Limani <limani.samy@courrier.uqam.ca>
2025-06-24 20:44:37 -0400

    Set up grpc_client.py routes.py

---
+++ file_service/app/routes.py
@@ -0,0 +1,20 @@
+from fastapi import APIRouter, File, UploadFile
+import os
+from grpc_client import send_file_to_parser
+
+router = APIRouter()
+
+UPLOAD_DIR = "storage"
+os.makedirs(UPLOAD_DIR, exist_ok=True)
+
+@router.post("/upload/")
+async def upload_file(file: UploadFile = File(...)):
+    file_location = os.path.join(UPLOAD_DIR, file.filename)
+    with open(file_location, "wb") as buffer:
+        content = await file.read()
+        buffer.write(content)
+    try:
+        send_file_to_parser(file_location)
+    except Exception as e:
+        print(e)
+    return {"filename": file.filename, "path": file_location}
