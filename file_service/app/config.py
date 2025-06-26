00a7f2c2158ac3085d66a2ea7f48540c9de961eb
Samy Limani <limani.samy@courrier.uqam.ca>
2025-06-26 17:15:44 -0400

    Set up config params in config.py

---
+++ file_service/app/config.py
@@ -0,0 +1,8 @@
+
+MAX_FILE_SIZE = 25 * 1024 * 1024     # 25 Mb
+
+ALLOWED_MIME_TYPES = {
+    "application/pdf",
+    "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+    "text/plain"
+}
