0cc1ad2fd9a2ea2ff95f6f15e511ec838eb4fc29
Samy Limani <limani.samy@courrier.uqam.ca>
2025-07-10 02:51:19 -0400

    Refactor GRPCClient in grpc_client.py

--- file_service/app/grpc_client.py
+++ file_service/app/grpc_client.py
@@ -1,17 +1,23 @@
 import grpc
-import os
-import parser_pb2, parser_pb2_grpc
+
+from app.config.settings import Settings
+from app.grpc import parser_pb2, parser_pb2_grpc
+from app.interfaces.interfaces import ParserClient
+
+STORAGE_DIR = Settings.UPLOAD_DIR
+GRPC_SERVER_HOST = Settings.GRPC_SERVER_HOST
+GRPC_SERVER_PORT = Settings.GRPC_SERVER_PORT
 
 
-STORAGE_DIR = "storage"
 
-def send_file_to_parser(filename, storage_dir=STORAGE_DIR):
-    file_path = os.path.join(storage_dir, filename)
-    with open(file_path, "rb") as f:
-        file_bytes = f.read()
 
-    channel = grpc.insecure_channel("localhost:50051")
-    stub = parser_pb2_grpc.ResumeParserStub(channel)
-    request = parser_pb2.ParseResumeRequest(filename=filename, file_content=file_bytes)
-    response = stub.ParseResume(request)
-    return response.text
+class GRPCClient(ParserClient):
+    def __init__(self, host: str = Settings.GRPC_SERVER_HOST, port: int = GRPC_SERVER_PORT):
+        self.target = f"{host}:{port}"
+        self.channel = grpc.insecure_channel(self.target)
+        self.stub = parser_pb2_grpc.ResumeParserStub(self.channel)
+
+    def send_to_parser(self, file_url: str) -> str:
+        request = parser_pb2.ParseResumeRequest(file_url=file_url)
+        response = self.stub.ParseResume(request)
+        return response.text
