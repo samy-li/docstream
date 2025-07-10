5457250d8c3c92836f5d9800c72d78dc610d9908
Samy Limani <limani.samy@courrier.uqam.ca>
2025-07-10 02:08:58 -0400

    Add doc to interfaces.py

--- file_service/app/interfaces/interfaces.py
+++ file_service/app/interfaces/interfaces.py
@@ -3,9 +3,28 @@ from abc import ABC, abstractmethod
 class FileStorage(ABC):
     @abstractmethod
     def save_file(self, filename: str, content: bytes) -> str:
+        """
+             Save a file to the configured storage system.
+
+             Args:
+                 filename (str): The object name to assign.
+                 content (bytes): The file's binary content.
+
+             Returns:
+                 str: storage path
+        """
         pass
 
 class ParserClient(ABC):
     @abstractmethod
     def send_to_parser(self, file_url: str) -> str:
+        """
+               Send a saved file to parser-service.
+
+               Args:
+                   file_url (str): path to the file in storage.
+
+               Returns:
+                   str: Parser response payload.
+        """
         pass
