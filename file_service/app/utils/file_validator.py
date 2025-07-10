ac24956863f32c5a35ed5a31b87ac519c1189a17
Samy Limani <limani.samy@courrier.uqam.ca>
2025-07-10 02:28:03 -0400

    Add doc to file_validator.py

--- file_service/app/utils/file_validator.py
+++ file_service/app/utils/file_validator.py
@@ -1,14 +1,32 @@
 from fastapi import HTTPException
 import magic
-from file_service.app.config import MAX_FILE_SIZE, ALLOWED_MIME_TYPES
+from app.config.settings import MAX_FILE_SIZE, ALLOWED_MIME_TYPES
 
-def validate_file_size(content: bytes):
+def validate_file_size(content: bytes) -> bool:
+    """
+       Validates that the uploaded file does not exceed the max allowed size.
+
+       Args:
+           content (bytes): The raw file content.
+
+       Returns:
+           bool: True if size is within limits, False otherwise.
+    """
     if len(content) > MAX_FILE_SIZE:
         return False
     else:
         return True
 
 def validate_file_type(content: bytes):
+    """
+       Validates the MIME type of the file using libmagic.
+
+       Args:
+           content (bytes): The raw file content.
+
+       Returns:
+           bool: True if the MIME type is allowed, False otherwise.
+    """
     mime = magic.from_buffer(content, mime=True)
     if mime not in ALLOWED_MIME_TYPES:
         return False
@@ -16,12 +34,21 @@ def validate_file_type(content: bytes):
         return True
 
 def validate_file(content: bytes):
+    """
+        Validates both the size and MIME a file.
+
+        Raises:
+            HTTPException: 413 if file too large, 400 if unsupported MIME type.
+
+        Returns:
+            bool: True if all validations pass.
+    """
     if not validate_file_size(content):
         raise HTTPException(status_code=413,
-                            detail="content too large")
+                            detail="File too large")
     if not validate_file_type(content):
         raise HTTPException(status_code=400,
-                            detail="Invalid content type")
+                            detail="Invalid file type")
     return True
 
 
