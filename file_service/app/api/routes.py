24ab0e96f3262ef3b389232af6c4e303a4c84a19
Samy Limani <limani.samy@courrier.uqam.ca>
2025-07-10 01:16:54 -0400

    Add documentation to routes.py

--- file_service/app/api/routes.py
+++ file_service/app/api/routes.py
@@ -1,12 +1,12 @@
 import os
+from fastapi import APIRouter, File, UploadFile, Depends
 from typing import Annotated
 
 from app.config.settings import Settings
-from fastapi import APIRouter, File, UploadFile, Depends
+from app.auth.gateway_user_provider import get_current_user_id
 from app.core.upload_service import UploadService
 from app.schemas.responses import UploadResponse
 from dependencies.upload_service_di import get_upload_service
-from app.auth.gateway_user_provider import get_current_user_id
 
 router = APIRouter()
 
@@ -25,9 +25,24 @@ async def upload_file(file: Annotated[UploadFile, File(...)],
                       upload_service: Annotated[UploadService, Depends(
                           get_upload_service)]
                       ) -> UploadResponse:
+    """
+     Uploads a document, stores it, and sends it for parsing.
+
+     The document is received, validated, saved,
+     then forwarded to the parser-service.
+
+     Args:
+         file: Supported formats: PDF, DOCX, TXT,...etc.
+         user_id: extracted from gateway header.
+         upload_service: Service responsible for file upload workflow.
+
+     Returns:
+         UploadResponse: Object containing file name and parser response.
+     """
     response = await upload_service.handle_upload(file, user_id)
 
-    return UploadResponse(filename=response["filename"], parser_response=response["parser_response"])
+    return UploadResponse(filename=response["filename"],
+                          parser_response=response["parser_response"])
 
 
 @router.get("/health",
@@ -36,4 +51,10 @@ async def upload_file(file: Annotated[UploadFile, File(...)],
             tags=["System"]
             )
 def health():
+    """
+       Returns OK if service is up, otherwise an error is raised.
+
+       Returns:
+           dict: { "status": "ok" }
+       """
     return {"status": "ok"}
