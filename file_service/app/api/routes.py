ebcf7fd9cc8344d5dc5db4e886858ed1968e266b
Samy Limani <limani.samy@courrier.uqam.ca>
2025-07-10 01:00:40 -0400

    Add pydantic validation to routes.py

--- file_service/app/api/routes.py
+++ file_service/app/api/routes.py
@@ -1,8 +1,10 @@
 import os
 from app.config.settings import Settings
 from fastapi import APIRouter, File, UploadFile, Depends
+from lxml.parser import filename
 
 from app.core.upload_service import UploadService
+from app.schemas.responses import UploadResponse
 from dependencies.upload_service_di import get_upload_service
 from app.auth.gateway_user_provider import get_current_user_id
 
@@ -17,11 +19,10 @@ async def upload_file(file: UploadFile = File(...),
                       user_id: str = Depends(get_current_user_id),
                       upload_service: UploadService = Depends(
                           get_upload_service)
-                      ):
+                      ) -> UploadResponse:
     response = await upload_service.handle_upload(file, user_id)
 
-    return {"filename": response["file_name"],
-            "parser_response": response["parser_response"]}
+    return UploadResponse(filename=response["filename"], parser_response=response["parser_response"])
 
 
 @router.get("/health")
