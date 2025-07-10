d31b5d1167645901506bdba67adff2e007d36266
Samy Limani <limani.samy@courrier.uqam.ca>
2025-07-10 00:46:06 -0400

    Refactored routes.py

--- file_service/app/api/routes.py
+++ file_service/app/api/routes.py
@@ -1,45 +1,29 @@
-from fastapi import APIRouter, File, UploadFile, Header, HTTPException, Depends
 import os
-from app.auth.jwt_utils import verify_token, create_token
-from app.utils.file_validator import validate_file
-from app.grpc_client import send_file_to_parser
+from app.config.settings import Settings
+from fastapi import APIRouter, File, UploadFile, Depends
+
+from app.core.upload_service import UploadService
+from dependencies.upload_service_di import get_upload_service
+from app.auth.gateway_user_provider import get_current_user_id
 
 router = APIRouter()
 
-UPLOAD_DIR = "storage"
+UPLOAD_DIR = Settings.UPLOAD_DIR
 os.makedirs(UPLOAD_DIR, exist_ok=True)
 
 
-def get_current_user(authorization: str = Header(...)):
-    if not authorization.startswith("Bearer "):
-        raise HTTPException(status_code=401, detail="Missing Bearer token")
-    token = authorization.split(" ")[1]
-    try:
-        return verify_token(token)
-    except Exception as e:
-        raise HTTPException(status_code=401, detail=str(e))
-
-
-@router.post("/token")
-def get_token(username: str):
-    return {"access_token": create_token(username), "token_type": "bearer"}
-
-
 @router.post("/upload")
-async def upload_file(file: UploadFile = File(...), user_id: str = Depends(get_current_user)):
-    file_location = os.path.join(UPLOAD_DIR, file.filename)
-    response = ""
-    content = await file.read()
-    validate_file(content)
-    with open(file_location, "wb") as buffer:
-            buffer.write(content)
-    try:
-        response = send_file_to_parser(file.filename)
-    except Exception as e:
-        print(e)
-    return {"filename": file.filename, "path": file_location, "parser_response": response}
+async def upload_file(file: UploadFile = File(...),
+                      user_id: str = Depends(get_current_user_id),
+                      upload_service: UploadService = Depends(
+                          get_upload_service)
+                      ):
+    response = await upload_service.handle_upload(file, user_id)
+
+    return {"filename": response["file_name"],
+            "parser_response": response["parser_response"]}
+
 
 @router.get("/health")
 def health():
     return {"status": "ok"}
-
