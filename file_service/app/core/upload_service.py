f7a2491dcdd7ca8708e7bd61ade70d80a95f694a
Samy Limani <limani.samy@courrier.uqam.ca>
2025-07-10 01:58:24 -0400

    Add documentation to UploadService

--- file_service/app/core/upload_service.py
+++ file_service/app/core/upload_service.py
@@ -1,6 +1,8 @@
 from fastapi import UploadFile
-from app.utils.file_validator import validate_file
+
 from app.interfaces.interfaces import FileStorage, ParserClient
+from app.utils.file_validator import validate_file
+
 
 class UploadService:
     def __init__(self, storage: FileStorage, parser: ParserClient):
@@ -8,13 +10,25 @@ class UploadService:
         self.parser = parser
 
     async def handle_upload(self, file: UploadFile, user_id: str):
+        """
+               Handles file validation, saving to MinIO, and sending to parser.
+
+               Args:
+                   file: The uploaded file.
+                   user_id: User ID to scope the file path.
+
+               Returns:
+                   dict: { filename, path, parser_response }
+        """
         content = await file.read()
         validate_file(content)
 
         # Scoped filename prevents collisions between users
         scoped_filename = f"{user_id}/{file.filename}"
+
         path = self.storage.save_file(scoped_filename, content)
         response = self.parser.send_to_parser(path)
+
         return {
             "filename": file.filename,
             "path": path,
