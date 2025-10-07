9d24506eaa50a562ce8b134578dda31a5240b12e
Samy Limani <limani.samy@courrier.uqam.ca>
2025-10-07 17:37:07 -0400

    Declare auth_service schemas.py

---
+++ auth_service/app/schemas/schemas.py
@@ -0,0 +1,10 @@
+from pydantic import BaseModel
+
+class TokenRequest(BaseModel):
+    username: str
+    password: str
+
+class TokenResponse(BaseModel):
+    access_token: str
+    token_type: str
+    expires_in: int
