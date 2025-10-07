e4f7929bdaa947bd7c49916b6f1089279c367bff
Samy Limani <limani.samy@courrier.uqam.ca>
2025-10-07 18:04:17 -0400

    Update imports in main.py in parser_service

--- parser_service/main.py
+++ parser_service/main.py
@@ -1,11 +1,11 @@
 import grpc
 from concurrent import futures
-from server import ResumeParser
-from proto import parser_pb2_grpc
+from app.clients.server import DocumentParser
+from app.proto import parser_pb2_grpc
 
 def run():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    parser_pb2_grpc.add_ResumeParserServicer_to_server(ResumeParser(), server)
+    parser_pb2_grpc.add_ResumeParserServicer_to_server(DocumentParser(), server)
     server.add_insecure_port('[::]:50051')
     print("parser_service running on port 50051...")
     server.start()
