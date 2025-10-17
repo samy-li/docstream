78783c4143143259b2b2208e866583bb40ef6afa
Samy Limani <limani.samy@courrier.uqam.ca>
2025-10-16 23:50:24 -0400

    Delete unnecessary files

--- file_service/app/config.py
+++
@@ -1,8 +0,0 @@
-
-MAX_FILE_SIZE = 10 * 1024 * 1024     # 10 Mb
-
-ALLOWED_MIME_TYPES = {
-    "application/pdf",
-    "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
-    "text/plain"
-}
