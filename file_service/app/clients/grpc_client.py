70e43de78345086d9bc661d3790ec7efac3fb8bd
Samy Limani <limani.samy@courrier.uqam.ca>
2025-10-16 23:59:49 -0400

    Integrate circuit breaker to gRPC client in file_service

--- file_service/app/clients/grpc_client.py
+++ file_service/app/clients/grpc_client.py
@@ -1,9 +1,15 @@
+import logging
+
 import grpc
+from pybreaker import CircuitBreakerError
 
 from app.config.settings import get_settings
 from app.grpc import parser_pb2, parser_pb2_grpc
 from app.interfaces.interfaces import ParserClient
+from app.resilience.factory.breaker_factory import parser_breaker
 
+
+logger = logging.getLogger(__name__)
 
 class GRPCClient(ParserClient):
     """
@@ -35,5 +41,24 @@ class GRPCClient(ParserClient):
                    str: The text content returned by the parser.
         """
         request = parser_pb2.ParseResumeRequest(file_url=file_url)
-        response = self.stub.ParseResume(request)
+
+        try:
+            # RungRPC call through the circuit breaker
+            return parser_breaker.call(self._invoke_grpc, request)
+
+        except CircuitBreakerError:
+            # Circuit is OPEN → skip calling parser_service
+            logger.warning(
+                f"[CircuitBreaker] parser_service unavailable — skipping {file_url}")
+            raise
+
+        except grpc.RpcError as e:
+            logger.error(f"gRPC communication error with parser_service: {e}")
+            raise
+    def _invoke_grpc(self, request):
+        response = self.stub.ParseResume(request, timeout=30)
         return response.text
+
+    def __del__(self):
+        if hasattr(self, 'channel'):
+            self.channel.close()
